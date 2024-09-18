e008e9c839eb94be40275e52f97c94d22e669512
kun <kun@myshell.ai>
2024-09-19 00:33:35 +0800

    feat: copy widget

---
+++ web/apps/web/src/components/workflow/nodes/widget-node/hook/use-paste-state.tsx
@@ -0,0 +1,72 @@
+import {
+  useReactFlowStore,
+  getCanvasCenter,
+  uuid,
+  NodeId,
+} from '@shellagent/flow-engine';
+import { useKeyPress } from 'ahooks';
+import { useCallback } from 'react';
+
+import {
+  filterVariable,
+  getKeyboardKeyCodeBySystem,
+  isEventTargetInputArea,
+} from '@/utils/common-helper';
+import { useWorkflowStore } from '@/stores/workflow/workflow-provider';
+import { useWorkflowState } from '@/stores/workflow/use-workflow-state';
+
+export const usePasteState = ({
+  enabeKeyboard = false,
+}: {
+  enabeKeyboard: boolean;
+}) => {
+  const { setNodeData, nodeData } = useWorkflowStore(state => ({
+    setNodeData: state.setNodeData,
+    nodeData: state.nodeData,
+  }));
+
+  const { reactFlowWrapper, viewport, onAddNode, nodes } = useReactFlowStore(
+    state => ({
+      reactFlowWrapper: state.reactFlowWrapper,
+      viewport: state.viewport,
+      onAddNode: state.onAddNode,
+      nodes: state.nodes,
+    }),
+  );
+
+  const { currentCopyId } = useWorkflowState(state => ({
+    currentCopyId: state.currentCopyId,
+  }));
+
+  const pasteState = useCallback(
+    (id: string) => {
+      const newId = uuid() as NodeId;
+
+      setNodeData({ id: newId, data: filterVariable(nodeData[id]) });
+      const position = getCanvasCenter(reactFlowWrapper, viewport);
+      const data = nodes.find(item => item.id === id) as any;
+      onAddNode({
+        type: data.data.type,
+        position,
+        data: {
+          id: newId,
+          name: data?.data?.name || '',
+          display_name: data?.data?.name || '',
+          type: 'widget',
+        },
+      });
+    },
+    [setNodeData, nodeData, reactFlowWrapper, viewport, onAddNode],
+  );
+
+  useKeyPress(`${getKeyboardKeyCodeBySystem('ctrl')}.v`, e => {
+    if (isEventTargetInputArea(e.target as HTMLElement)) {
+      return;
+    }
+    if (currentCopyId && enabeKeyboard) {
+      pasteState(currentCopyId);
+    }
+  });
+
+  return { pasteState };
+};
