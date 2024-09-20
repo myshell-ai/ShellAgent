8f14ae2b5fdee7b488818a4e24ecef2330b8c223
Wenliang Zhao <zhaowl20@mails.tsinghua.edu.cn>
2024-09-20 20:06:35 +0800

    Dev update libraries (#18)
    Co-authored-by: yuxumin <1090414006@qq.com>

---
+++ .ci/windows_base_files/run.bat
@@ -0,0 +1,44 @@
+@echo off
+setlocal
+chcp 65001 >nul
+:: Check for administrator privileges
+
+:: Attempt to create a directory in the system folder, which requires admin privileges
+mkdir "%windir%\System32\dummy" >nul 2>&1
+
+:: Check if the last operation was successful
+if '%errorlevel%' == '0' (
+    :: Remove the created directory
+    rmdir "%windir%\System32\dummy"
+) else (
+    echo Please run as administrator. 请以管理员方式运行
+    pause
+    exit /b
+)
+
+cd /d %~dp0
+cd ShellAgent
+
+del "proconfig\widgets\imagen_widgets\library\comfy_custom_nodes\ComfyUI_FizzNodes\javascript\Folder here to satisfy init, eventually I'll have stuff in here..txt"
+
+set PATH=..\git\bin;%PATH%
+set MYSHELL_KEY=OPENSOURCE_FIXED
+
+git config --global core.longpaths true
+
+if not exist "output" (
+    mkdir "output"
+)
+
+if not exist "servers\web" (
+    mkdir -p "servers\web"
+)
+
+if not exist "models\model_status.json" (
+    echo {} > "models\model_status.json"
+)
+
+..\python_embeded\python.exe -m pip install -e .
+..\python_embeded\python.exe servers\main.py
+
+pause
