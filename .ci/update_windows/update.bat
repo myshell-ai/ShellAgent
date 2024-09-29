45b30fea17ea204e8e539aef841ee0e4806b3922
Chengwei Ouyang <118652142+Cherwayway@users.noreply.github.com>
2024-09-29 10:24:10 +0800

    Auto update (#37)

---
+++ .ci/update_windows/update.bat
@@ -0,0 +1,8 @@
+@echo off
+..\python_embeded\python.exe .\update.py ..\ShellAgent\
+if exist update_new.py (
+  move /y update_new.py update.py
+  echo Running updater again since it got updated.
+  ..\python_embeded\python.exe .\update.py ..\ComfyUI\ --skip_self_update
+)
+if "%~1"=="" pause
