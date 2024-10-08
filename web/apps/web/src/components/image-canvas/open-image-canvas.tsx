b7e64415ec072db4f0b412dff6df838cd348d930
shanexi <shane@myshell.ai>
2024-10-08 23:33:48 +0800

    feat: use button to display open image canvas

--- web/apps/web/src/components/image-canvas/open-image-canvas.tsx
+++ web/apps/web/src/components/image-canvas/open-image-canvas.tsx
@@ -1,3 +1,18 @@
+import { PhotoIcon } from '@heroicons/react/24/outline';
+import { Button } from '@shellagent/ui';
+
 export function OpenImageCanvas() {
-  return <span>Open Image Canvas</span>;
+  return (
+    <Button
+      icon={PhotoIcon}
+      onClick={() => {
+        console.log('clicked open image canvas');
+      }}
+      variant="link"
+      size="sm"
+      type="button"
+      className="w-18 border-default rounded-full">
+      Open Image Canvas
+    </Button>
+  );
 }
