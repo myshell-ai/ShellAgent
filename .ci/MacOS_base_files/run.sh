871df2f542beaecce1991495981d72c2419dd14d
yuxumin <1090414006@qq.com>
2024-10-17 00:22:48 +0800

    update

--- .ci/MacOS_base_files/run.sh
+++ .ci/MacOS_base_files/run.sh
@@ -1,10 +1,17 @@
-VENV_NAME="shellagent"
-VENV_PATH="./$VENV_NAME"
+#!/bin/bash
 
-python -m venv "$VENV_PATH"
-source "$VENV_PATH/bin/activate
+VENV_PATH=python_venv
+
+if [ -d $VENV_PATH ]; then
+    source $VENV_PATH/bin/activate
+else
+    python -m venv $VENV_PATH
+    source $VENV_PATH/bin/activate
+fi
+
+echo "activate python from $(which python)"
 
 cd ShellAgent
-python -m pip install poetry
-python -m pip install -e .
+python -m pip install poetry 
+python -m pip install --use-pep517 -e . 
 python servers/main.py
