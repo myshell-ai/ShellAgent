7e0c48434e404f423bc4fbfabed0914b208e4c66
yuxumin <1090414006@qq.com>
2024-10-16 22:42:56 +0800

    udpate

--- .ci/MacOS_base_files/run.sh
+++ .ci/MacOS_base_files/run.sh
@@ -1,3 +1,3 @@
 cd ShellAgent
-../python_embeded/python3.10/bin/pip3 install -e .
+../python_embeded/python3.10/bin/python3 -m pip install -e .
 ../python_embeded/python3.10/bin/python3 servers/main.py
