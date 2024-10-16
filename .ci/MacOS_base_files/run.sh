186046bfdc6ea038df88155c03db36b59845762e
yuxumin <1090414006@qq.com>
2024-10-16 22:33:30 +0800

    update

--- .ci/MacOS_base_files/run.sh
+++ .ci/MacOS_base_files/run.sh
@@ -1,3 +1,3 @@
 cd ShellAgent
-pip3 install -e .
-../Python-3.10.10/python3.10/bin/python3.10 servers/main.py
+../python_embeded/python3.10/bin/pip3 install -e .
+../python_embeded/python3.10/bin/python3 servers/main.py
