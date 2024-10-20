8046e07d87a9c544256663987d4acf83df9ed219
shanexi <shane@myshell.ai>
2024-10-20 11:02:04 +0800

    Revert "39 software update (#43)"
    This reverts commit b4bb20e1042836b7fb1eef64584301ae4bebdbbc.

--- web/apps/web/src/components/settings/settings-sidebar.tsx
+++ web/apps/web/src/components/settings/settings-sidebar.tsx
@@ -8,7 +8,6 @@ import { useInjection } from 'inversify-react';
 import { observer } from 'mobx-react-lite';
 
 import { SettingsModel, SidebarValue } from './settings.model';
-import { Badge } from 'antd';
 
 export const SettingsSideBar = observer(() => {
   const model = useInjection(SettingsModel);
@@ -29,15 +28,6 @@ export const SettingsSideBar = observer(() => {
         onValueChange={value => model.setSidebar(value as SidebarValue)}
         className="w-full h-full p-0">
         <TabsList className="flex flex-col h-full items-start justify-start p-0 gap-y-1.5">
-          <Badge dot={model.checkRet.has_new_stable}>
-            <TabsTrigger
-              value="SoftwareUpdate"
-              className="w-full justify-start text-subtler rounded-md border-b-0 mx-0 px-2 py-1 data-[state=active]:bg-surface-accent-blue-subtler">
-              {model.checkRet.has_new_stable
-                ? 'Software Update'
-                : 'Software Update'}
-            </TabsTrigger>
-          </Badge>
           <TabsTrigger
             value="Environment"
             className="w-full justify-start text-subtler rounded-md border-b-0 mx-0 px-2 py-1 data-[state=active]:bg-surface-accent-blue-subtler">
