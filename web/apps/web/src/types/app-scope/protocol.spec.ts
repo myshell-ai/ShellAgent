cb7f13806054269125f8f6ca3f3020a5417290ba
shanexi <shane@myshell.ai>
2024-10-29 09:53:57 +0800

    Test protocol

--- web/apps/web/src/types/app-scope/protocol.spec.ts
+++ web/apps/web/src/types/app-scope/protocol.spec.ts
@@ -1,5 +1,163 @@
+import { z } from 'zod';
+import { customKeySchema, variableSchema, variablesSchema } from './protocol';
+
 describe('protocol', () => {
-  it('test', () => {
-    expect(true).toBeTruthy();
+  it('test variable', () => {
+    variableSchema.parse({
+      type: 'text',
+      value: 'hello',
+    });
+
+    variableSchema.parse({
+      type: 'text',
+      value: {
+        type: 'text',
+        value: 'hello',
+      },
+    });
+
+    expect(() => {
+      variableSchema.parse({
+        type: 'text_not',
+        value: 'hello',
+      });
+    }).toThrowErrorMatchingInlineSnapshot(`
+          "[
+            {
+              "received": "text_not",
+              "code": "invalid_enum_value",
+              "options": [
+                "text",
+                "image",
+                "audio",
+                "video",
+                "file",
+                "text_file"
+              ],
+              "path": [
+                "type"
+              ],
+              "message": "Invalid enum value. Expected 'text' | 'image' | 'audio' | 'video' | 'file' | 'text_file', received 'text_not'"
+            }
+          ]"
+        `);
   });
+
+  it('test custom key', () => {
+    expect(() => {
+      customKeySchema.parse('type');
+    }).toThrowErrorMatchingInlineSnapshot(`
+      "[
+        {
+          "code": "custom",
+          "message": "Is a reserved key",
+          "path": []
+        }
+      ]"
+    `);
+
+    expect(() => {
+      customKeySchema.parse('id');
+    }).toThrowErrorMatchingInlineSnapshot(`
+      "[
+        {
+          "code": "custom",
+          "message": "Is a reserved key",
+          "path": []
+        }
+      ]"
+    `);
+
+    expect(() => {
+      customKeySchema.parse('properties');
+    }).toThrowErrorMatchingInlineSnapshot(`
+      "[
+        {
+          "code": "custom",
+          "message": "Is a reserved key",
+          "path": []
+        }
+      ]"
+    `);
+
+    expect(() => {
+      customKeySchema.parse('payload');
+    }).toThrowErrorMatchingInlineSnapshot(`
+      "[
+        {
+          "code": "custom",
+          "message": "Is a reserved key",
+          "path": []
+        }
+      ]"
+    `);
+
+    customKeySchema.parse('123');
+    customKeySchema.parse('type1');
+    customKeySchema.parse('a');
+    customKeySchema.parse('ttt');
+    customKeySchema.parse('1a');
+    customKeySchema.parse('a_1');
+    customKeySchema.parse('1.a');
+    customKeySchema.parse('type.a');
+    customKeySchema.parse('a.1a');
+    customKeySchema.parse('.a');
+    customKeySchema.parse('a.1a');
+  });
+
+  it('test state variables', () => {
+    variablesSchema.parse({
+      test: {
+        type: 'text',
+        value: 'test',
+      },
+    });
+
+    expect(() => {
+      variablesSchema.parse({
+        properties: {
+          type: 'text',
+          value: 'test',
+        },
+      });
+    }).toThrowErrorMatchingInlineSnapshot(`
+      "[
+        {
+          "code": "custom",
+          "message": "Is a reserved key",
+          "path": [
+            "properties"
+          ]
+        }
+      ]"
+    `);
+
+    /*     expect(() => {
+          variablesSchema.parse({
+            Properties: {
+              type: 'text',
+              value: 'test',
+            },
+          });
+        }).toThrowErrorMatchingInlineSnapshot(`
+          "[
+            {
+              "code": "custom",
+              "message": "Is a reserved key",
+              "path": [
+                "Properties"
+              ]
+            }
+          ]"
+        `); */
+  });
+
+  it('test CustomKey', () => {
+    const tag = z.string();
+    tag.parse('Hello');
+    const parsed = tag.safeParse('Hello');
+    expect(parsed).toMatchInlineSnapshot('');
+  });
+
+  it('test state', () => {});
 });
---
+++ web/apps/web/src/types/app-scope/protocol.ts
@@ -0,0 +1,63 @@
+import { CustomKey } from '@shellagent/pro-config';
+import { z } from 'zod';
+
+export const variableTypeSchema = z.enum([
+  'text',
+  'image',
+  'audio',
+  'video',
+  'file',
+  'text_file',
+]);
+
+// https://github.com/colinhacks/zod/discussions/2245
+// https://github.com/colinhacks/zod?tab=readme-ov-file#recursive-types
+const baseVariableSchema = z.object({
+  type: variableTypeSchema,
+});
+
+export type Variable = z.infer<typeof baseVariableSchema> & {
+  value: Variable | string;
+};
+
+export const variableSchema: z.ZodType<Variable> = baseVariableSchema.extend({
+  value: z.union([z.lazy(() => variableSchema), z.string()]),
+});
+
+// pro-config-type/common.ts#7
+export const reservedKeySchema = z.enum([
+  'type',
+  'id',
+  'properties',
+  'inputs',
+  'outputs',
+  'tasks',
+  'render',
+  'transitions',
+  'states',
+  'context',
+  'payload',
+]);
+
+export const customKeySchema = z
+  .string()
+  .refine(val => !reservedKeySchema.safeParse(val).success, {
+    message: `Is a reserved key`,
+  }) satisfies z.Schema<CustomKey>;
+
+export const variablesSchema = z.record(customKeySchema, variableSchema);
+
+export const ouputNameSchema = z.union([customKeySchema, z.string()]);
+
+export const stateSchema = z.object({
+  variables: variablesSchema,
+  children: z.object({
+    inputs: z.object({
+      variables: variablesSchema,
+    }),
+    tasks: z.object({
+      variables: variablesSchema,
+    }),
+    outputs: z.object({}),
+  }),
+});
