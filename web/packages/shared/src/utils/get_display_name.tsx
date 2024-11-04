375b759b784d6f20579678695bc1689779735e96
joe <joe@myshell.ai>
2024-11-04 10:53:22 +0800

    fix: deps cycle

--- web/packages/shared/src/utils/get_display_name.tsx
+++ web/packages/shared/src/utils/get_display_name.tsx
@@ -1,6 +1,28 @@
 import { Task } from '@/protocol/task';
 import { Button } from '@/protocol/render-button';
-import { WidgetItem, NodeTypeEnum } from '@shellagent/flow-engine';
+
+export enum NodeTypeEnum {
+  start = 'start',
+  end = 'end',
+  widget = 'widget',
+  state = 'state',
+  workflow = 'workflow',
+}
+
+export type NodeType = keyof typeof NodeTypeEnum;
+
+export type WidgetItem = {
+  display_name: string; // widget显示名称
+  name: string; // widget名称(唯一标识)
+  icon?: string; // icon
+  desc?: string; // widget描述
+  children?: WidgetItem[]; // 子widget
+  type?: NodeType;
+  custom?: boolean; // 是否为自定义widget
+  undraggable?: boolean; // 是否可以拖拽至画布中
+  customRender?: () => JSX.Element;
+  widget_name?: string;
+};
 
 export const getTaskDisplayName = (value: WidgetItem, tasks: Task[]) => {
   let indexMap: Record<string, number> = {};
