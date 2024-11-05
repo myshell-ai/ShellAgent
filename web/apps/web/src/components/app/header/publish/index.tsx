6209cc67cce6eeacf36878f2a7a379dd31c86813
kun <kun@myshell.ai>
2024-11-05 17:56:25 +0800

    feat: publish

--- web/apps/web/src/components/app/header/publish/index.tsx
+++ web/apps/web/src/components/app/header/publish/index.tsx
@@ -32,6 +32,10 @@ import { saveApp, releaseApp, fetchAppVersionList } from '@/services/app';
 import { genAutomata } from '@/stores/app/utils/data-transformer';
 import { cn } from '@/utils/cn';
 import { Metadata } from '@/services/home/type';
+import {
+  GetAppVersionListResponse,
+  GetAppVersionListRequest,
+} from '@/services/app/type';
 
 import VersionSkeleton from '../skeleton';
 
@@ -44,6 +48,116 @@ interface PublishProps {
   flowInstance: AppState['flowInstance'];
   metadata: Metadata;
 }
+
+const DropdownRender = ({
+  versionData,
+  getVersionLoading,
+  app_id,
+  versionName,
+  setVersionName,
+  handleRelease,
+  releaseLoading,
+  handleSave,
+  saveLoading,
+}: {
+  versionData?: GetAppVersionListResponse;
+  getVersionLoading: boolean;
+  app_id: string;
+  versionName: string;
+  setVersionName: React.Dispatch<React.SetStateAction<string>>;
+  handleRelease: () => Promise<void>;
+  releaseLoading: boolean;
+  handleSave: () => void;
+  saveLoading: boolean;
+}) => {
+  return (
+    <div className="w-80 text-xs mx-4 p-3 text-left rounded-lg border border-opaque shadow-modal-default outline-none data-[state=open]:animate-in [&[data-state=open]>span]:animate-none data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 bg-surface-search-field text-subtle">
+      <div className="flex flex-col gap-3">
+        <Heading size="h4">Version</Heading>
+        <Input
+          placeholder="please enter version name"
+          value={versionName}
+          size="xs"
+          className="col-span-3"
+          onChange={e => setVersionName(e.target.value)}
+        />
+        {versionName ? (
+          <Button
+            onClick={handleRelease}
+            size="md"
+            className="w-full"
+            loading={releaseLoading}>
+            Save As
+          </Button>
+        ) : (
+          <Button
+            onClick={handleSave}
+            size="md"
+            className="w-full"
+            loading={saveLoading}>
+            Save
+          </Button>
+        )}
+      </div>
+      <Separator className="my-3" />
+      <Heading size="h4">History Versions</Heading>
+      <ScrollArea
+        className={cn(
+          'w-full',
+          Array.isArray(versionData?.data) && versionData?.data?.length > 3
+            ? 'h-80'
+            : 'h-52',
+        )}>
+        {getVersionLoading ? (
+          <VersionSkeleton />
+        ) : (
+          versionData?.data?.map(item => {
+            if (item.version_name === 'latest') {
+              return (
+                <div className="p-1.5 rounded-md hover:bg-surface-hovered cursor-not-allowed">
+                  <Text color="subtle" className="block w-full">
+                    {`${item.version_name}(current)`}
+                  </Text>
+                  {/* {autoSavedTime && !version_name ? (
+                  <Text size="sm" color="subtlest">
+                    Updated {dayjs(autoSavedTime).fromNow()}
+                  </Text>
+                ) : null} */}
+                </div>
+              );
+            }
+            return (
+              <Link
+                className="flex justify-between items-center p-1.5 rounded-md hover:bg-surface-hovered group/menu"
+                href={`${window.location.pathname}?id=${app_id}&version_name=${item.version_name}`}
+                prefetch={false}>
+                <div>
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Text color="subtle" className="block w-64 truncate">
+                          {item.version_name}
+                        </Text>
+                      </TooltipTrigger>
+                      <TooltipContent>{item.version_name}</TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                  <Text size="sm" color="subtlest">
+                    Published {dayjs(item.create_time * 1000).fromNow()}
+                  </Text>
+                </div>
+                <ArrowUpRightIcon className="w-4.5 h-4.5 hidden group-hover/menu:flex" />
+              </Link>
+            );
+          })
+        )}
+        {!getVersionLoading && isEmpty(versionData?.data) ? (
+          <Text>No Data</Text>
+        ) : null}
+      </ScrollArea>
+    </div>
+  );
+};
 
 export default function Publish({
   app_id,
@@ -234,99 +348,19 @@ export default function Publish({
         </>
       ) : (
         <Dropdown.Button
-          // open={showPublishPopover}
           onOpenChange={onOpenChange}
           dropdownRender={() => (
-            <div className="w-80 text-xs mx-4 p-3 text-left rounded-lg border border-opaque shadow-modal-default outline-none data-[state=open]:animate-in [&[data-state=open]>span]:animate-none data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 bg-surface-search-field text-subtle">
-              <div className="flex flex-col gap-3">
-                <Heading size="h4">Version</Heading>
-                <Input
-                  placeholder="please enter version name"
-                  value={versionName}
-                  size="xs"
-                  className="col-span-3"
-                  onChange={e => setVersionName(e.target.value)}
-                />
-                {versionName ? (
-                  <Button
-                    onClick={handleRelease}
-                    size="md"
-                    className="w-full"
-                    loading={releaseLoading}>
-                    Save As
-                  </Button>
-                ) : (
-                  <Button
-                    onClick={handleSave}
-                    size="md"
-                    className="w-full"
-                    loading={saveLoading}>
-                    Save
-                  </Button>
-                )}
-              </div>
-              <Separator className="my-3" />
-              <Heading size="h4">History Versions</Heading>
-              <ScrollArea
-                className={cn(
-                  'w-full',
-                  Array.isArray(versionData?.data) &&
-                    versionData?.data?.length > 3
-                    ? 'h-80'
-                    : 'h-52',
-                )}>
-                {getVersionLoading ? (
-                  <VersionSkeleton />
-                ) : (
-                  versionData?.data?.map(item => {
-                    if (item.version_name === 'latest') {
-                      return (
-                        <div className="p-1.5 rounded-md hover:bg-surface-hovered cursor-not-allowed">
-                          <Text color="subtle" className="block w-full">
-                            {`${item.version_name}(current)`}
-                          </Text>
-                          {/* {autoSavedTime && !version_name ? (
-                          <Text size="sm" color="subtlest">
-                            Updated {dayjs(autoSavedTime).fromNow()}
-                          </Text>
-                        ) : null} */}
-                        </div>
-                      );
-                    }
-                    return (
-                      <Link
-                        className="flex justify-between items-center p-1.5 rounded-md hover:bg-surface-hovered group/menu"
-                        href={`${window.location.pathname}?id=${app_id}&version_name=${item.version_name}`}
-                        prefetch={false}>
-                        <div>
-                          <TooltipProvider>
-                            <Tooltip>
-                              <TooltipTrigger asChild>
-                                <Text
-                                  color="subtle"
-                                  className="block w-64 truncate">
-                                  {item.version_name}
-                                </Text>
-                              </TooltipTrigger>
-                              <TooltipContent>
-                                {item.version_name}
-                              </TooltipContent>
-                            </Tooltip>
-                          </TooltipProvider>
-                          <Text size="sm" color="subtlest">
-                            Published {dayjs(item.create_time * 1000).fromNow()}
-                          </Text>
-                        </div>
-                        <ArrowUpRightIcon className="w-4.5 h-4.5 hidden group-hover/menu:flex" />
-                      </Link>
-                    );
-                  })
-                )}
-                {!getVersionLoading && isEmpty(versionData?.data) ? (
-                  <Text>No Data</Text>
-                ) : null}
-              </ScrollArea>
-            </div>
+            <DropdownRender
+              versionData={versionData}
+              getVersionLoading={getVersionLoading}
+              app_id={app_id}
+              versionName={versionName}
+              setVersionName={setVersionName}
+              handleRelease={handleRelease}
+              releaseLoading={releaseLoading}
+              handleSave={handleSave}
+              saveLoading={saveLoading}
+            />
           )}
           buttonsRender={() => {
             return [
