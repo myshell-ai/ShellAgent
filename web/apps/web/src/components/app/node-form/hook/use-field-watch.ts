1bf80245d43ea85d85b697ea1651d0733f72834c
joe <joe@myshell.ai>
2024-11-07 16:49:36 +0800

    input ref

--- web/apps/web/src/components/app/node-form/hook/use-field-watch.ts
+++ web/apps/web/src/components/app/node-form/hook/use-field-watch.ts
@@ -7,8 +7,9 @@ import { AppBuilderModel } from '@/components/app/app-builder.model';
 
 import { FormRef } from '@shellagent/ui';
 import { useEffect, useRef, useCallback } from 'react';
-import { get, set, cloneDeep, isEqual, debounce } from 'lodash-es';
+import { get, set, cloneDeep, isEqual, debounce, isString } from 'lodash-es';
 import { replaceKey, getDiffPath, DiffTypeEnum } from './form-utils';
+import { useSchemaContext } from '@/stores/app/schema-provider';
 
 export function useFieldWatch(
   formRef: React.RefObject<FormRef>,
@@ -18,6 +19,8 @@ export function useFieldWatch(
 ) {
   const prevValuesRef = useRef<Record<string, TValues | undefined>>({});
   const appBuilder = useInjection(AppBuilderModel);
+
+  const stateId = useSchemaContext(state => state.id);
 
   // 处理 context 变化的函数
   const handleContextChange = useCallback(
@@ -45,7 +48,10 @@ export function useFieldWatch(
         const prevContextValue: TValues = get(prevValue, name);
         const newContextValue: TValues = get(newValue, name);
 
-        if (prevContextValue?.name && newContextValue?.name) {
+        if (
+          isString(prevContextValue?.name) &&
+          isString(newContextValue?.name)
+        ) {
           if (prevContextValue.name !== newContextValue.name) {
             const oldKey = name.split('.')[1];
             const newKey = customSnakeCase(newContextValue.name || '');
@@ -72,23 +78,58 @@ export function useFieldWatch(
 
   // 处理 inputs 变化的函数
   const handleInputsChange = useCallback(
-    debounce((newValue: TValues, prevValue: TValues, inputsKey: string) => {
-      // 补充
-      const oldInputs = get(prevValue, ['inputs', inputsKey]);
-      const newInputs = get(newValue, ['inputs', inputsKey]);
+    debounce((newValue: TValues, prevValue: TValues, name: string) => {
+      if (name === reservedKeySchema.Enum.inputs) {
+        const oldInputs: TValues = get(prevValue, name);
+        const newInputs: TValues = get(newValue, name);
+        const diffPath = getDiffPath(oldInputs, newInputs);
+        diffPath.forEach(diff => {
+          switch (diff.type) {
+            case DiffTypeEnum.Deleted:
+              appBuilder.hanldeRefScene({
+                scene: RefSceneEnum.Enum.remove_ref_opts,
+                params: {
+                  paths: [`${stateId}.${name}.${diff.path}`],
+                },
+              });
+              break;
+            default:
+              break;
+          }
+        });
+      } else {
+        const inputName = name.split('.')[1];
+        const prevInputsValue: TValues = get(prevValue, [
+          reservedKeySchema.Enum.inputs,
+          inputName,
+        ]);
+        const newInputsValue: TValues = get(newValue, [
+          reservedKeySchema.Enum.inputs,
+          inputName,
+        ]);
 
-      if (oldInputs?.name && newInputs?.name) {
-        if (oldInputs?.name !== newInputs?.name) {
-          replaceKey(formRef, {
-            parentPath: reservedKeySchema.Enum.inputs,
-            oldKey: inputsKey,
-            newKey: customSnakeCase(newInputs?.name || ''),
-            value: newInputs,
-          });
+        if (isString(prevInputsValue?.name) && newInputsValue?.name) {
+          if (prevInputsValue.name !== newInputsValue.name) {
+            const newKey = customSnakeCase(newInputsValue.name || '');
+            appBuilder.hanldeRefScene({
+              scene: RefSceneEnum.Enum.rename_ref_opt,
+              params: {
+                oldPath: `${stateId}.${reservedKeySchema.Enum.inputs}.${inputName}`,
+                newPath: `${stateId}.${reservedKeySchema.Enum.inputs}.${newKey}`,
+              },
+            });
+            // todo 会触发DiffTypeEnum.Deleted
+            replaceKey(formRef, {
+              parentPath: reservedKeySchema.Enum.inputs,
+              oldKey: inputName,
+              newKey,
+              value: newInputsValue,
+            });
+          }
         }
       }
     }, 300),
-    [],
+    [stateId, appBuilder.hanldeRefScene],
   );
 
   // 处理 outputs 变化的函数
@@ -160,9 +201,8 @@ export function useFieldWatch(
 
     if (name.startsWith(reservedKeySchema.Enum.context)) {
       handleContextChange(newValue, prevValue, name);
-    } else if (name.startsWith(`${reservedKeySchema.Enum.inputs}.`)) {
-      const inputsKey = name.split('.')[1];
-      handleInputsChange(newValue, prevValue, inputsKey);
+    } else if (name.startsWith(reservedKeySchema.Enum.inputs)) {
+      handleInputsChange(newValue, prevValue, name);
     } else if (name.startsWith(reservedKeySchema.Enum.outputs)) {
       const outputsKey = name.split('.')[1];
       handleOutputsChange(newValue, prevValue, outputsKey);
