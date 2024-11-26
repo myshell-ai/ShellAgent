bc88c8be71a0c0cdd371955a369ffae0996a88cf
kun <kun@myshell.ai>
2024-11-26 17:04:36 +0800

    feat: bot ui

--- web/apps/web/src/components/assistant/index.tsx
+++ web/apps/web/src/components/assistant/index.tsx
@@ -20,17 +20,17 @@ const Assistant = observer(() => {
         : 0, // 计算初始 x 位置
     y:
       typeof window !== 'undefined'
-        ? window.innerHeight - Number(drawerSize.height) - 72
+        ? window.innerHeight - Number(drawerSize.height) - 64
         : 0, // 计算初始 y 位置
   });
 
   return (
     <>
-      <div className="fixed right-6 bottom-8">
+      <div className="fixed right-6 bottom-3">
         <IconButton
           icon={() => <Bot className="w-9 h-9" />}
           variant="outline"
-          className="w-9 h-9 text-brand bg-white shadow-button-basic"
+          className="w-11 h-11 text-brand bg-white shadow-button-basic"
           color="default"
           onClick={model.drawer.open}
           autoFocus={false}
@@ -62,7 +62,7 @@ const Assistant = observer(() => {
           <Drawer
             width="100%"
             height="100%"
-            className="rounded-lg"
+            className="rounded-2xl"
             headerClassName="px-4 !py-3 bg-surface-container-default rnd-drag-header cursor-move"
             contentClassName="!p-0"
             onClose={model.drawer.close}
