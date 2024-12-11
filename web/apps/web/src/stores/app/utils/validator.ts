985a75f212fc23a0e39f730eb299d4ace4327bfc
kun <kun@myshell.ai>
2024-12-11 16:43:09 +0800

    feat: form validator

--- web/apps/web/src/stores/app/utils/validator.ts
+++ web/apps/web/src/stores/app/utils/validator.ts
@@ -2,58 +2,52 @@ export async function validateImage(
   image: string | string[],
   maxSize: number = 1000000,
 ): Promise<boolean | string> {
-  return new Promise(async (resolve, reject) => {
-    let url;
+  let url;
 
-    if (Array.isArray(image)) {
-      if (!image.length) {
-        resolve(true);
-      } else if (image.length < 2) {
-        url = image[0];
-      } else {
-        reject(new Error('Avoid using multiple images in the intro message.'));
-        return;
-      }
+  if (Array.isArray(image)) {
+    if (!image.length) {
+      return Promise.resolve(true);
+    } else if (image.length < 2) {
+      url = image[0];
     } else {
-      url = image;
+      return Promise.reject(
+        new Error('Avoid using multiple images in the intro message.'),
+      );
     }
-    if (!url) {
-      resolve(true);
-      return;
-    }
-    const apiUrl = url?.startsWith('/api/files') ? url : `/api/files/${url}`;
-    try {
-      // 校验文件大小
-      const response = await fetch(apiUrl, { method: 'GET' });
-      const contentLength = response.headers.get('content-length');
-      if (contentLength) {
-        const fileSize = parseInt(contentLength, 10);
-        if (fileSize > maxSize) {
-          return reject(new Error('Keep image file size under 1MB.'));
-        }
+  } else {
+    url = image;
+  }
+  if (!url) {
+    return Promise.resolve(true);
+  }
+  const apiUrl = url?.startsWith('/api/files') ? url : `/api/files/${url}`;
+  try {
+    const response = await fetch(apiUrl, { method: 'GET' });
+    const contentLength = response.headers.get('content-length');
+    if (contentLength) {
+      const fileSize = parseInt(contentLength, 10);
+      if (fileSize > maxSize) {
+        return Promise.reject(new Error('Keep image file size under 1MB.'));
       }
-      // 校验图片尺寸
-      const image = new Image();
-      image.src = apiUrl;
-      const imageLoaded = new Promise<void>(() => {
-        image.onload = () => {
-          if (image.width < image.height) {
-            reject(
-              new Error('Avoid portrait-shaped images for the intro message.'),
-            );
-          } else {
-            resolve(true);
-          }
-        };
-        image.onerror = () => console.log('Failed to load image.');
-      });
-      await imageLoaded;
-      resolve(true);
-      return;
-    } catch (error) {
-      console.log('error: ', error);
-      // reject('Validation failed.');
-      return;
     }
-  });
+    const image = new Image();
+    image.src = apiUrl;
+    const imageLoaded = new Promise<void>((resolve, reject) => {
+      image.onload = () => {
+        if (image.width < image.height) {
+          reject(
+            new Error('Avoid portrait-shaped images for the intro message.'),
+          );
+        } else {
+          resolve();
+        }
+      };
+      image.onerror = () => console.log('Failed to load image.');
+    });
+    await imageLoaded;
+    return Promise.resolve(true);
+  } catch (error: any) {
+    console.log('error: ', error);
+    return Promise.reject(error.message || error);
+  }
 }
