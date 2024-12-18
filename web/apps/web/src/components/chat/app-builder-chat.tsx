78b4cc9c21a32ff926b42e7ba1e6c79980dd5028
shanexi <shane@myshell.ai>
2024-12-18 13:58:27 +0800

    Fix type

--- web/apps/web/src/components/chat/app-builder-chat.tsx
+++ web/apps/web/src/components/chat/app-builder-chat.tsx
@@ -8,17 +8,20 @@ import {
 } from 'myshell-bundled-chat';
 import { NextIntlClientProvider } from 'next-intl';
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import { AppBuilderChatModel } from './app-builder-chat.model';
 
 export const AppBuilderChat = () => {
   const model = useInjection(AppBuilderChatModel);
+  const traceId = uuidv4();
   return (
     <ChakraProvider>
       <NextIntlClientProvider locale="zh" messages={en}>
         <GlobalStoreProvider
           nonce="ZmEyZjU3NzktNTQ0NS00Y2YxLTgxMjctODMyM2JlNTVjZDEx"
-          isMobile={false}>
+          isMobile={false}
+          traceId={traceId}>
           <UserStoreProvider user={model.chatNew.user} isLogin language="en">
             <ChatStoreProvider>
               <ChatNew model={model.chatNew} />
